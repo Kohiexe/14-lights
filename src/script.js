6e5e68630d3e61b8e7dc2401b5f2e0ac4760d021
Kohiexe <croisssss@gmail.com>
2023-01-11 14:03:13 +0100

    changed text

--- src/script.js
+++ src/script.js
@@ -37,7 +37,7 @@ fontLoader.load(
 
         // Text
         const textGeometry = new TextGeometry(
-            'Hello Three.js',
+            'Kell Beans',
             {
                 font: font,
                 size: 0.5,
